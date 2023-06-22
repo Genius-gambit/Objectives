4fc517dcd77d783c57d992e9e84162caeff18aef
Muazzam Akhtar <81755254+Genius-gambit@users.noreply.github.com>
2023-06-22 14:31:05 +0400

    Create diamond.c

---
+++ diamond.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+char *diamond (int n)
+{
+  char  *str
+  if ((n % 2 == 0) || n <= 0)
+    return (NULL);
+  if (n == 1)
+  {
+    str = (char *)calloc(3, sizeof(char));
+	str[0] = '*';
+	str[1] = '\n';
+	str[2] = 0;
+	return(str);
+  }
+  else
+  {
+	int	stars = n + 1;
+
+  }
+// allocate a string on the heap, memory will be freed
+	// return calloc(1, 1);
+}
