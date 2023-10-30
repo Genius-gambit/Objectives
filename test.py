f583880a9df9932d873a2b7fcfdf9aa334586351
Muazzam Akhtar <81755254+Genius-gambit@users.noreply.github.com>
2023-10-30 17:17:50 +0400

    Add files via upload

--- test.py
+++ test.py
@@ -1,21 +1,78 @@
+import cv2
+import argparse
+
 from ultralytics import YOLO
-# import gi
+import supervision as sv
+import numpy as np
 
-# gi.require_version('Gst', '1.0')
-# from gi.repository import Gst, GObject
 
-# GObject.threads_init()
-# Gst.init(None)
+ZONE_POLYGON = np.array([
+    [0, 0],
+    [0.5, 0],
+    [0.5, 1],
+    [0, 1]
+])
 
-# pipeline_description = "rtspsrc location=rtsp://10.10.30.99:554/media/video1 latency=10 ! rtph264depay ! h264parse ! omxh264dec ! nvoverlaysink overlay-x=400 overlay-y=0 overlay-w=1280 overlay-h=720 overlay=2"
-# pipeline = Gst.parse_launch(pipeline_description)
 
-# pipeline.set_state(Gst.State.PLAYING)
-# model = YOLO("yolov8n.pt")
-# results = model(source=pipeline,save=True,save_txt=False,conf=0.4,project='detection_results')
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="YOLOv8 live")
+    parser.add_argument(
+        "--webcam-resolution", 
+        default=[1280, 720], 
+        nargs=2, 
+        type=int
+    )
+    args = parser.parse_args()
+    return args
 
-# mainLoop = GObject.MainLoop()
-#try:
-    # mainLoop.run()
-# except  KeyboardInterrupt:
-    # pipeline.set_state(Gst.State.NULL)
+
+def main():
+    args = parse_arguments()
+    frame_width = 1280
+    frame_height = 720
+
+    cap = cv2.VideoCapture("rtsp://admin:Admin123.@10.10.30.100:554/Streaming/Channels/101")
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+
+    model = YOLO("yolov8n.pt")
+
+    box_annotator = sv.BoxAnnotator(
+        thickness=2,
+        text_thickness=2,
+        text_scale=1
+    )
+
+    zone_polygon = (ZONE_POLYGON * np.array(args.webcam_resolution)).astype(int)
+    zone = sv.PolygonZone(polygon=zone_polygon, frame_resolution_wh=tuple(args.webcam_resolution))
+    zone_annotator = sv.PolygonZoneAnnotator(
+        zone=zone, 
+        color=sv.Color.red(),
+        thickness=2,
+        text_thickness=4,
+        text_scale=2
+    )
+
+    while True:
+        ret, frame = cap.read()
+
+        result = model(frame, agnostic_nms=True)[0]
+        detections = sv.Detections.from_ultralytics(result)
+        labels = []
+        for detection in detections.__iter__():
+            _str = f"{model.model.names[detection[3]]} " + str(round(detection[2], 2))
+            labels.append(_str)
+        print(labels)
+        frame = box_annotator.annotate(scene=frame, detections=detections, labels=labels)
+
+        zone.trigger(detections=detections)
+        frame = zone_annotator.annotate(scene=frame)
+        try:
+        	cv2.imshow("yolov8", frame)
+        	cv2.waitKey(4)
+        except:
+        	print("Here")
+
+
+if __name__ == "__main__":
+    main()
